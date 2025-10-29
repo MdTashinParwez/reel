3f71324b69dd623699e491615c285bd90a7b6171
tashinparwez537@gmail.com <tashinparwez537@gmai.com>
2025-10-29 16:55:44 +0530

    first commit

---
+++ models/videos.ts
@@ -0,0 +1,39 @@
+import mongoose, {Schema,model,models} from "mongoose";
+
+
+export const VIDEO_DIMENSION= {
+    width: 1080,
+    height: 1920,
+} as const;
+
+export interface IVideo{
+    _id?: mongoose.Types.ObjectId
+    title: string;
+    description: string;
+    videoUrl: string;
+    thubnailUrl: string;
+    controls?: boolean;
+    transformation?: {
+        height: number;
+        width: number;
+        quality?: number
+    }
+
+
+}
+const videoSchema = new Schema<IVideo>({
+  title:{ type: String, required: true },
+  description:{ type: String, required: true },
+  videoUrl:{ type: String, required: true },
+  thubnailUrl:{ type: String, required: true },
+  controls:{ type: Boolean, required: true },
+  transformation:{
+    height:{ type: Number, default: VIDEO_DIMENSION.height },
+    width:{ type: Number, default: VIDEO_DIMENSION.width },
+    quality:{ type: Number, min:1 , max: 100, default: 80 },
+  }
+},{timestamps:true});
+
+const Video = models?.Video ||  model<IVideo>("Video", videoSchema);
+
+export default Video;
