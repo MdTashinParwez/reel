605086c66a899ef5482e4cf285ecc80effc179ec
tashinparwez537@gmail.com <tashinparwez537@gmai.com>
2026-03-14 21:37:44 +0530

    checking error

--- type.d.ts
+++ type.d.ts
@@ -1,6 +1,6 @@
-import { Connection } from "mongoose"
+import { Connection } from "mongoose"  // import the Connection type from mongoose
 
-declare global{
+declare global{ // declare a global variable named mongoose with the specified type
     var mongoose: {
         conn : Connection | null
         promise: Promise<Connection> | null
